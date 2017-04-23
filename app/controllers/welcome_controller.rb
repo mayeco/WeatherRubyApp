0211dc458cee5a2873821265d504656643445628
Mario Young <maye.co@gmail.com>
2017-04-22 22:22:46 -0300

    fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,51 @@
+require 'unirest'
+
 class WelcomeController < ApplicationController # :nodoc:
+
+  @@valid_city = {
+      'paris,fr': 'Paris',
+      'bangkok,th': 'Bangkok',
+      'santiago,cl': 'Santiago',
+      'new york,ny': 'New York',
+      'beijing,cn': 'Hong Kong',
+      'bamako,ml': 'Bamako',
+  }
+
   def index # :nodoc:
-    require 'unirest'
+    aerisapi_client_id = 'Rs9CyAFqVXwV3okIPCZE5'
+    aerisapi_client_secret = '71X02kz3RZ4AZkNRBohwXGyajlEDgs7EtAm3zLUj'
+    secret = 'client_id=' + aerisapi_client_id + '&client_secret=' + aerisapi_client_secret
+    url_base = 'https://api.aerisapi.com/forecasts/%s?from=today&to=today&'
 
-    @welcome = 'hello world'.to_s
+    @weather = {}
 
-    data = Unirest.get'https://httpbin.org/get'
+    @@valid_city.each do |city|
+      url = sprintf url_base + secret, city[0]
+      response = Unirest.get url
+      data = JSON.parse(response.raw_body)
 
-    @response = JSON.parse(data.raw_body)
+      if not data['success']
+        raise 'Error in request'
+      end
+
+      data = data['response']
+      data = data[0]
+      data = data['periods']
+      data = data[0]
+
+      @weather[city[0]] = data
+    end
+
+    @valid_city = @@valid_city
+
+  end
+
+  def city_weather # :nodoc:
+    @city = params['city']
+
+    if not @@valid_city.keys.include?(@city.to_sym)
+      raise 'Not valid city'
+    end
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get '/', to: 'welcome#index', as: 'homepage'
 
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/:city', to: 'welcome#city_weather', as: 'city_weather_name'
 end
